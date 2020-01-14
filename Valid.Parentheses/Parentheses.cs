b02d09ea414518ff406a9c21cd6f1dcc2c99334a
Yura Bobrykov <iurii.bobrykov@gmail.com>
2020-01-15 07:33:09 +1300

    added solution for valid parentheses

--- Valid.Parentheses/Parentheses.cs
+++ Valid.Parentheses/Parentheses.cs
@@ -7,6 +7,7 @@ namespace Valid.Parentheses
 		public static bool ValidParentheses(string input)
 		{
 			var inputLength = input.Length;
+			var invalid = false;
 
 			if (input.Count(i => i == '(' || i == ')') % 2 != 0 || inputLength < 0 || inputLength > 100 ||
 			    input.StartsWith(')') || input.EndsWith('(') ||
@@ -15,7 +16,29 @@ namespace Valid.Parentheses
 				return false;
 			}
 
-			return true;
+			var opened = 0;
+			var closed = 0;
+
+			foreach (var el in input.ToCharArray())
+			{
+				if (el == '(')
+				{
+					opened++;
+				}
+
+				if (el == ')')
+				{
+					closed++;
+				}
+
+				if (closed > opened)
+				{
+					invalid = true;
+					break;
+				}
+			}
+
+			return !invalid;
 		}
 	}
 }
